37dcb0b7bb939f25122d9cd01be638619d2debdf
Kevin O'Connor <kco5107@psu.edu>
2024-09-18 17:08:44 -0400

    pysocket basics

---
+++ Client.py
@@ -0,0 +1,94 @@
+import socket
+import sys
+import selectors
+import types
+
+HOST = "127.0.0.1"  # The server's hostname or IP address, 127.0.0.1 is localhost 
+PORT = 65432  # The port used by the server
+
+"""
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b"Hello, world")
+    data = s.recv(1024)
+"""
+    
+sel = selectors.DefaultSelector()
+messages = [b"Message 1 from client.", b"Message 2 from client."]
+
+def start_connections(host, port, num_conns):
+    server_addr = (host, port)
+    for i in range (0, num_conns):
+        connid = i + 1
+        print(f"Starting Connection {connid} to {server_addr}")
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+        sock.connect_ex(server_addr)
+
+
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        data = types.SimpleNamespace(
+            connid = connid,
+            msg_total = sum(len(m) for m in messages),
+            recv_total = 0,
+            messages = messages.copy(),
+            outb = b"",
+        )
+
+        sel.register(sock, events, data = data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+
+    if mask & selectors.EVENT_READ:
+        recv_data = sock.recv(1024)
+
+        if recv_data:
+            print(f"Received {recv_data!r} from connection {data.connid}")
+            data.recv_total += len(recv_data)
+
+        try:
+            sel.unregister(sock)
+            sock.close()
+        except Exception as e:
+            print(f"Error during cleanup: {e}")
+    
+    if mask & selectors.EVENT_WRITE:
+        if not data.outb and data.messages: #No outgoing data but messages to send, Pop next message and add it to out list
+            data.outb = data.messages.pop(0)
+        if data.outb: #Send any outgoing data if there is any
+            print(f"Sending {data.outb!r} to connection {data.connid}")
+            sent = sock.send(data.outb) #should be ready to write
+            data.outb = data.outb[sent: ]
+
+
+def event_loop():
+    try:
+        while True:
+            # Exit event loop there are no currently registered sockets to avoid errors
+            if not sel.get_map().keys():
+                print("No sockets Registered")
+                break
+            events = sel.select(timeout=None)
+            for key, mask in events:
+                service_connection(key, mask)
+            
+                
+    except KeyboardInterrupt:
+        print("caught Keyboard Interrupt, Exiting")
+    finally:
+        sel.close()
+
+
+
+if __name__ == "__main__":
+    #Start connections (e.g. 2 clients)
+    start_connections(HOST, PORT, num_conns=2)
+
+    event_loop()
+
+
+
+#print(f"Received {data!r}")
--- Server.py
+++ Server.py
@@ -1,11 +1,95 @@
 import socket
 import threading
+import selectors
+import types
 
-def start_server(server_port):
+sel = selectors.DefaultSelector()
+
+
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
+PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
+
+
+lsock =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+lsock.bind((HOST, PORT))
+    
+print("Listening on " +  str(PORT))
+lsock.listen()
+
+lsock.setblocking(False)
+sel.register(lsock, selectors.EVENT_READ, data=None)
+
+
+
+def accept_wrapper(sock):
+    conn, addr = sock.accept() #Socket should already be read to read if this fn is called
+    print(f"Accepted connection from {addr}")
+    conn.setblocking(False)
+    data = types.SimpleNamespace(addr = addr, inb = b"", outb = b"")
+    events = selectors.EVENT_READ | selectors.EVENT_WRITE
+    sel.register(conn, events, data = data)
+
+
+def service_connection(key, mask):
+    sock = key.fileobj
+    data = key.data
+    print(data)
+
+    if mask & selectors.EVENT_READ:
+        #try:
+        recv_data = sock.recv(1024) #Should be ready to read
+        #except Exception as e:
+         #   print("Unhandled Read Error")
+            
+        if recv_data:
+            data.outb += recv_data
+        else:
+            print(f"Closing connection {data.addr}")
+            sel.unregister(sock)
+            sock.close()
+
+    if mask & selectors.EVENT_WRITE:
+        if data.outb:
+            print(f"Echoing {data.outb!r} to {data.addr}")
+            sent = sock.send(data.outb) #Should be ready to write
+            data.outb = data.outb[sent:]
+
+
+
+try:
+    while True:
+        events = sel.select(timeout = None)
+        for key, mask in events:
+            if key.data is None:
+                accept_wrapper(key.fileobj)
+            else:
+                service_connection(key, mask)
+except KeyboardInterrupt:
+    print("Caught keyboard interrupt, exiting")
+finally:
+    sel.close()
+
+
+
+
+"""
+    conn, addr = s.accept()
+    with conn:
+        print(f"connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
+"""
+
+#def start_server(server_port):
     
 
 
-
+""""
 if __name__ == "__main__":
     SERVER_PORT = 8000
     start_server(SERVER_PORT)
+
+"""
