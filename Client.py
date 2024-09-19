f01f67b7755dfcd3ba083ee8be0edf39cd126fd8
Kevin O'Connor <kco5107@psu.edu>
2024-09-18 20:07:04 -0400

    Establishing Connection progress  on Client

--- Client.py
+++ Client.py
@@ -2,6 +2,7 @@ import socket
 import sys
 import selectors
 import types
+import errno
 
 HOST = "127.0.0.1"  # The server's hostname or IP address, 127.0.0.1 is localhost 
 PORT = 65432  # The port used by the server
@@ -15,6 +16,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     
 sel = selectors.DefaultSelector()
 messages = [b"Message 1 from client.", b"Message 2 from client."]
+
+
 
 def start_connections(host, port, num_conns):
     server_addr = (host, port)
@@ -36,6 +39,7 @@ def start_connections(host, port, num_conns):
         )
 
         sel.register(sock, events, data = data)
+
 
 
 def service_connection(key, mask):
@@ -64,6 +68,7 @@ def service_connection(key, mask):
             data.outb = data.outb[sent: ]
 
 
+
 def event_loop():
     try:
         while True:
@@ -80,6 +85,42 @@ def event_loop():
         print("caught Keyboard Interrupt, Exiting")
     finally:
         sel.close()
+
+
+#This function should take in an ip and port number and return a TCP socket connection to that IP/Port
+def open_connection(ip, port_number):
+        server_addr = (ip, port_number)
+        print(f"Starting Connection to {server_addr}")
+
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.setblocking(False)
+
+        #Attempt to connect
+        result = sock.connect_ex(server_addr)
+
+        if result == 0:
+            print(f"Connection to {server_addr} successful immediately")
+
+        elif result == errno.EINPROGRESS or result == errno.EWOULDBLOCK:
+            print(f"Connection to {server_addr} in progress")
+
+        else:
+            print(f"Error connecting to {server_addr}")
+            sock.close()
+            return None
+
+        events = selectors.EVENT_READ | selectors.EVENT_WRITE
+        sel.register(sock, events, data = None)
+
+        return sock
+
+def close_connection(sock):
+    sel.unregister(sock)
+    sock.close()
+
+
+#This function should receive the information of an open socket connection. It should also receive 
+def send_message(sock, version, msg_type, payload):
 
 
 
