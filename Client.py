3df9ba65fc0d69c9c9925c818e902fc5f89aa6cb
Kevin O'Connor <kco5107@psu.edu>
2024-09-20 11:47:06 -0400

    Cleint receive message code

--- Client.py
+++ Client.py
@@ -3,6 +3,7 @@ import sys
 import selectors
 import types
 import errno
+import struct
 
 HOST = "127.0.0.1"  # The server's hostname or IP address, 127.0.0.1 is localhost 
 PORT = 65432  # The port used by the server
@@ -47,7 +48,7 @@ def open_connection(ip, port_number):
             type = "sock",
             endpoint = server_addr, #Which peer/server the connection is to
             incoming_buffer = b'',
-            msg_length = 0, #to record how many bytes we should expect an incoming message to be (so we know if we have received it all)
+            messageLength = None, #to record how many bytes we should expect an incoming message to be (so we know if we have received it all)
 
             outgoing_buffer =  b'',
         )
@@ -76,6 +77,21 @@ def handle_connection(key, mask):
             print(f"Received: {data}")
             data.incoming_buffer += received
 
+            #If we don't know the incoming message length yet. We should try to read it
+            if data.messageLength is None and len(data.incoming_buffer >= 4):
+                #We can extract first 4 bytes as this be the message length prefix
+                data.messageLength = struct.unpack('!I', data.incoming_buffer[:4])[0] #Network byte order?????
+                data.incoming_buffer = data.incoming_buffer[4:]
+                print(f"Expected Message Length {data.messageLength} bytes")
+
+            #If we do know the message length, we should process/clear incoming buffer once it has been fully received
+            if data.messageLength is not None and len(data.incoming_buffer) >= data.messageLength:
+                message = data.incoming_buffer[:data.messageLength]
+                #NEED TO PROCESS MESSAGE HERE
+                data.incoming_buffer = data.incoming_buffer[data.messageLength: ] #Remove the message from buffer
+                data.messageLength = None #Reset message length so that we know there's no message currently
+
+
             # For demonstration, we immediately echo back the received data
             #data.outgoing_buffer += received  # Add it to outgoing buffer to echo it back
         else: #If 0 bytes received, client closed connection
@@ -84,7 +100,7 @@ def handle_connection(key, mask):
             sock.close()
     
     if mask & selectors.EVENT_WRITE and data.outgoing_buffer:
-        sent = sock.send(data.outgoing_buffer) #Non-blocking send
+        sent = sock.send(data.outgoing_buffer) #Non-blocking send (Hopefully the message should have already been encoded prior to being put into the buffer)
         data.outgoing_buffer = data.outgoing_buffer[sent: ] #Remove sent part from the buffer
 
 
